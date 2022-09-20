71ffe0862de903e85eeca94f2122282eb9fc6aec
dmitridrozdov <dm.drozdov@gmail.com>
2022-09-20 11:16:25 +1000

    Update products.js

--- routes/products.js
+++ routes/products.js
@@ -1,10 +1,11 @@
 import express from 'express'
 
-import { getProducts, addProducts } from '../controllers/products.js'
+import { getProducts, addProducts, getALLProducts } from '../controllers/products.js'
 
 const router = express.Router()
 
 router.get('/', getProducts)
+router.get('/', getALLProducts)
 router.post('/', addProducts)
 // router.patch('/:id', updateAgent)
 // router.delete('/:id', deleteAgent)
