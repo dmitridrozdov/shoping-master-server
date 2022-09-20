50afaf384b0d98000bf82bdd07d9076defde8906
dmitridrozdov <dm.drozdov@gmail.com>
2022-09-20 11:16:19 +1000

    Update products.js

--- controllers/products.js
+++ controllers/products.js
@@ -9,6 +9,15 @@ export const getProducts = async (req, res) => {
     }
 }
 
+export const getALLProducts = async (req, res) => {
+    try {
+        const products = await Product.find()
+        res.status(200).json(products)
+    } catch(error) {
+        res.status(404).json({ message: error.message})
+    }
+}
+
 export const addProducts =  async (req, res) => {
     const product = req.body
     const newProduct = new Product(product)
