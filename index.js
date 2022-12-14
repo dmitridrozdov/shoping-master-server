7bb45ee8290749c383e2307b4f8242001b06d1e0
dmitridrozdov <dm.drozdov@gmail.com>
2022-12-14 16:40:15 +1100

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,10 @@ app.use('/currentlist', currentListRouts)
 
 const PORT = process.env.PORT || 5000
 
-mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+const connection_url = process.env.CONNECTION_URL
+console.log(typeof(connection_url))
+
+mongoose.connect(connection_url, {useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
     .catch((error) => console.log(error.message))
 
